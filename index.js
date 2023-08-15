9e2a462c3a33ccfc0928114ce4e02d7f9f3979e3
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2023-08-15 12:44:35 -0300

    random

---
+++ index.js
@@ -0,0 +1,15 @@
+let computerNumber
+let userNumbers = []
+
+// a função init
+function init() {
+    computerNumber = Math.floor(Math.random() * 100 + 1)
+    console.log(computerNumber);
+}
+
+
+
+function compareNummbers() {
+    const userNumber = Number(document.getElementById('inputBox').value)
+
+}
