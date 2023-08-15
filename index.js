15aa6a25caa484d902f2dccf22ec02691d566538
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2023-08-15 14:35:41 -0300

    adicionando o maximo de tentativas

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let computerNumber
 let userNumbers = []
+let attempts = 0
+let maxguesses = 10
 
 // a função init inicializadora vou jogar ela no meu body la no html 
 function init() {
     computerNumber = Math.floor(Math.random() * 100 + 1) // guardando dentro de uma variável 
-    console.log(computerNumber);
+
 }
 
 function compareNumbers() {
@@ -13,16 +15,28 @@ function compareNumbers() {
     document.getElementById('guesses').innerHTML = userNumbers
 
 
-    if (userNumber > computerNumber) {
-        document.getElementById('textOutput').innerHTML = 'Your number is too high'
+    if (attempts < maxguesses) {
+        if (userNumber > computerNumber) {
+            document.getElementById('textOutput').innerHTML = 'Your number is too high'
+            document.getElementById('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
 
-    }
-    else if (userNumber < computerNumber) {
-        document.getElementById('textOutput').innerHTML = 'Your number is too high'
+        }
+        else if (userNumber < computerNumber) {
+            document.getElementById('textOutput').innerHTML = 'Your number is too high'
+            document.getElementById('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
 
+        }
+        else {
+            document.getElementById('textOutput').innerHTML = 'Congratulations'
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+        }
     }
     else {
-        document.getElementById('textOutput').innerHTML = 'Congratulations'
+        document.getElementById('textOutput').innerHTML = 'You Lose! + The number computer was ' + computerNumber
     }
-
 }
