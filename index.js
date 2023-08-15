81a04bbb9d2d0b5109b059bc237a82255b871a67
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2023-08-15 13:55:53 -0300

    acrescentando ifs

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ let userNumbers = []
 
 // a função init inicializadora vou jogar ela no meu body la no html 
 function init() {
-    computerNumber = Math.floor(Math.random() * 100 + 1)
+    computerNumber = Math.floor(Math.random() * 100 + 1) // guardando dentro de uma variável 
     console.log(computerNumber);
 }
 
-
-
-function compareNummbers() {
+function compareNumbers() {
     const userNumber = Number(document.getElementById('inputBox').value)
+    userNumbers.push(' ' + userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
+
+
+    if (userNumber > computerNumber) {
+        document.getElementById('textOutput').innerHTML = 'Your number is too high'
+
+    }
+    else if (userNumber < computerNumber) {
+        document.getElementById('textOutput').innerHTML = 'Your number is too high'
+
+    }
+    else {
+        document.getElementById('textOutput').innerHTML = 'Congratulations'
+    }
 
 }
