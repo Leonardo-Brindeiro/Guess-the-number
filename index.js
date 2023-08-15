175e409c18636150f8115f1b70dac842c8fb163b
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2023-08-15 12:51:52 -0300

    guardando na variável e acrescentando o arredondamento flooor e o match random

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let computerNumber
 let userNumbers = []
 
-// a função init inicializadora 
+// a função init inicializadora vou jogar ela no meu body la no html 
 function init() {
     computerNumber = Math.floor(Math.random() * 100 + 1)
     console.log(computerNumber);
